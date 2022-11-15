937c3fe97fc71b93e2b1a9378bdb43a0e2991a1c
Anhw <rktsinger@gmail.com>
2022-11-15 19:29:59 +0900

    feat: GlobalStyle 추가 - GlobalStyle 컴포넌트 추가 - 의존성 변경 : styled-components 설치

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default GlobalStyle;
