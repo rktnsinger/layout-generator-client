569213ebb10fd4d38a6936959c3a626e8a02d88e
AnHyungwoo <96766719+rktnsinger@users.noreply.github.com>
2022-11-16 12:02:36 +0900

    feat: 로딩 컴포넌트 구현 (#5)
    - 스피너와 로딩 내용 표시
    
    Co-authored-by: Anhw <rktsinger@gmail.com>

---
+++ src/components/common/Loading/Loading.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+export default function Loading({ children }) {
+  return (
+    <Container>
+      <Spinner />
+      <Text>{children}</Text>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(82, 78, 79, 0.5);
+`;
+
+const rotateAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  align-self: center;
+  width: 100px;
+  height: 100px;
+  margin-top: 30vh;
+  border-top: 2px solid white;
+  border-right: 2px solid white;
+  border-bottom: 2px solid white;
+  border-left: 4px solid black;
+  border-radius: 50%;
+  background: transparent;
+  animation: ${rotateAnimation} 1s linear infinite;
+  transform: translateZ(0);
+`;
+
+const Text = styled.p`
+  align-self: center;
+  margin-top: 60px;
+  font-size: 60px;
+  font-weight: 700;
+  text-shadow: 3px 3px 5px gray;
+`;
