875441e966c34cd6f9763b726da269d71a18bdfc
Anhw <rktsinger@gmail.com>
2022-11-18 05:23:56 +0900

    feat: 이미지 레이아웃 인식 로직 1차 구현 - openCv를 활용하여 canny edge detection과 hough transform 적용 - 최소한의 기초적인 형태의 레이아웃만 인식하는 수준임. 계속해서 작업 필요 - 의존성 변경 : openCv를 js에서 활용하기위한 라이브러리 추가 - 위 라이브러리 추가시 webpack설정에 fallback 추가 - 이미지 처리 테스트를 위한 정적이미지들 추가

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "../styles/GlobalStyle";
-import MainPage from "../pages/MainPage";
+// import MainPage from "../pages/MainPage";
 
 import theme from "../styles/theme";
+import OpenCv from "../components/common/Canvas/OpenCv";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <MainPage />
+      {/* <MainPage /> */}
+      <OpenCv />
     </ThemeProvider>
   );
 }
