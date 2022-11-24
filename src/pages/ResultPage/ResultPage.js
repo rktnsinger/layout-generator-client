a7393b00fac41e8d2421fd529d9fdfe6bfcc26a1
Anhw <rktsinger@gmail.com>
2022-11-25 01:08:54 +0900

    feat: 결과 페이지 구현 - 인식된 레이아웃에 대한 코드 스니펫을 제공

---
+++ src/pages/ResultPage/ResultPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { Navigate, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import Description from "../../components/common/Description";
+import MainOperationButton from "../../components/common/MainOperationButton";
+
+import { generatedCodeState } from "../../recoil/store";
+import formatCode from "../../utils/formatCode";
+
+export default function ResultPage() {
+  const generatedCode = useRecoilValue(generatedCodeState);
+
+  const navigate = useNavigate();
+
+  const formattedCode = formatCode(generatedCode);
+
+  const handleRedirectHome = () => {
+    navigate("/", { replace: true });
+  };
+
+  if (!generatedCode) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <>
+      <Description>All done! Enjoy your work!</Description>
+      <Code>{formattedCode}</Code>
+      <MainOperationButton handleClick={handleRedirectHome}>
+        Go to Home
+      </MainOperationButton>
+    </>
+  );
+}
+
+const Code = styled.div`
+  margin-bottom: 80px;
+  padding: 30px;
+  background-color: lightgray;
+  width: 700px;
+  white-space: pre-wrap;
+`;
